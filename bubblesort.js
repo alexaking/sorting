7dc51cbfe74fe520f82aae017a5ad58618ea7a14
Emma Munley <emmamunley@gmail.com>
2019-10-22 12:50:54 -0400

    passing all specs :)

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,24 @@
-function bubbleSort(array) {
+const compareElements = (element1, element2) => element1 > element2;
 
-    /* your code here */
-    return array
+function swapElements(array, j, element1, element2) {
+  //swap elements
+  let temp = element1;
+  array[j] = element2;
+  array[j + 1] = temp;
+}
+
+function bubbleSort(array) {
+  if (array.length === 0 || array.length === 1) {
+    return array;
   }
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      let element1 = array[j];
+      let element2 = array[j + 1];
+      if (compareElements(element1, element2)) {
+        swapElements(array, j, element1, element2);
+      }
+    }
+  }
+  return array;
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,36 @@
-describe('Bubble Sort', function(){
-    it('handles an empty array', function(){
-      expect( bubbleSort([]) ).toEqual( [] );
-    });
+// beforeAll(function() {
+//   spyOn(window, 'swapElements').and.callThrough(); // replace existing `tootsiepop['lick']` method
+// });
+// it('swaps elements no more than 100 times', function() {
+//   window.swapElements();
+//   expect(swapElements.calls.count()).toBeLessThan(100);
+// });
+
+describe('Bubble Sort', function() {
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
   });
+  it('handles an array with 1 element', function() {
+    expect(bubbleSort([2])).toEqual([2]);
+  });
+  it('sorts an array of 2 elements', function() {
+    expect(bubbleSort([2, 1])).toEqual([1, 2]);
+  });
+  it('handles negative numbers', function() {
+    expect(bubbleSort([1, -1])).toEqual([-1, 1]);
+  });
+  it('sorts an array of 10 elements', function() {
+    expect(bubbleSort([23, 16, 8, 15, 4, 42, 10, -7, 0, 100])).toEqual([
+      -7,
+      0,
+      4,
+      8,
+      10,
+      15,
+      16,
+      23,
+      42,
+      100,
+    ]);
+  });
+});
